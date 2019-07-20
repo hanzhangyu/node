ff7b96614774ef3bd7ee9f111af4e1c033677fa0
PaulHan <zhangyu.han@outlook.com>
2019-07-20 23:28:51 +0800

    feat: same virtual addr in fork

--- playground/child_process/fork.c
+++ playground/child_process/fork.c
@@ -8,8 +8,9 @@ int main() {
     char *result = "init";
     // 1. 使用相同代码段
     // 2. 数据段与堆栈段复制一份至子进程
-    // 3. 而子进程的fork返回值为0
+    // 3. 而子进程的fork返回值为0，父进程返回子进程pid
     fpid = fork();
+    printf("fpid: %d", fpid);
     if (fpid < 0) {
         printf("error in fork");
         return 1;
@@ -17,12 +18,14 @@ int main() {
 
     if (fpid == 0) {
         printf("child pid: %d\n", getpid());
+        printf("child result addr: %p\n", &result); // 虚拟地址一致
         result = "changed";
         sleep(13);
         printf("result: %s\n", result);
         return 0;
     }
 
+    printf("parent result addr: %p\n", &result);
     printf("parent pid: %d\n", getpid());
     printf("result: %s\n", result);
     int status;
